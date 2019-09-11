b6a931d80ce6b6d67ac78890fc3800671e88f623
karenMora <karen.mora@mail.escuelaing.edu.co>
2019-09-11 10:51:51 -0500

    add lista parqueaderos

--- src/App.js
+++ src/App.js
@@ -11,6 +11,8 @@ import { RegistroParqueadero2 } from './App/RegisterComponent/RegistroParqueader
 import logo from './logo.svg';
 import './App.css';
 
+import ListaP from './App/RegisterComponent/ListaP';
+
 class App extends Component {
   constructor(props) {
     super(props);
@@ -28,7 +30,8 @@ class App extends Component {
           <BottomNavigation showLabels className="">
               <BottomNavigationAction label="Login" icon={<LockOpenIcon />} component={Link} to="/" />
               <BottomNavigationAction label="Register" icon={<AssignmentRoundedIcon />} component={Link} to="/todo" />
-			  <BottomNavigationAction label="Registro_Parqueadero" icon={<AssignmentRoundedIcon />} component={Link} to="/parqueadero" />
+			        <BottomNavigationAction label="Registro_Parqueadero" icon={<AssignmentRoundedIcon />} component={Link} to="/parqueadero" />
+              <BottomNavigationAction label="Lista parqueaderos" icon={<AssignmentRoundedIcon />} component={Link} to="/lista" />
           </BottomNavigation>
           <header className="App-header">
               <img src={logo} className="App-logo" alt="logo"/>
@@ -37,8 +40,10 @@ class App extends Component {
           <div>
               <Route exact path="/" component={LoginView}/>
               <Route path="/todo" component={RegisterView}/>
-			  <Route path="/parqueadero" component={RegistroPar1View}/>
-			  <Route path="/parqueadero2" component={RegistroParqueadero2View}/>
+              <Route path="/parqueadero" component={RegistroPar1View}/>
+              <Route path="/parqueadero2" component={RegistroParqueadero2View}/>
+              <Route path="/lista" component={listaView}/>
+
           </div>
         </div>
       </Router>
@@ -76,5 +81,36 @@ const RegistroParqueadero2View = () => (
       <RegistroParqueadero2 />
   </div>
 );
+
+const listaView = () => (
+  <div>
+    <h1 className="App-title">Lista de Parqueaderos </h1>
+    <br/>
+    <br/>
+    <ListaP Nombre="Parqueadero - Parque Simón Bolivar " Direccion="Cra. 59 #56-15 " Cupos=" 7"/>
+    <ListaP Nombre="PARQUEADERO PÚBLICO " Direccion="Cl. 93 #14-29 " Cupos=" 7"/>
+        <ListaP Nombre="Parqueadero Público Aparcar Ltda" Direccion="11, Cra. 13 #50 A " Cupos=" 7"/>
+        <ListaP Nombre="Parqueadero" Direccion="Cra. 13 #40A-17 " Cupos=" 9"/>
+        <ListaP Nombre="Parqueadero Privado de la Clínica Marly" Direccion="16, Cl. 50 #9 " Cupos=" 4"/>
+        <ListaP Nombre="Parqueadero" Direccion="Cl. 53 #1060 " Cupos=" 7"/>
+        <ListaP Nombre="parking" Direccion="Cl. 63 " Cupos=" 3"/>
+        <ListaP Nombre="Parqueadero Publico Eli García" Direccion="10, esquina con Carrera 8, Cl. 49 #7 " Cupos=" 2"/>
+        <ListaP Nombre="Parqueadero publico" Direccion="a 49-99, Cra. 13 #491 " Cupos=" 1"/>
+        <ListaP Nombre="Parqueadero La 51" Direccion="79, Cra. 13 #51 " Cupos=" 7"/>
+        <ListaP Nombre="Parqueadero Exito Cafam Cl 51" Direccion="Cl. 51 #1560 " Cupos=" 0"/>
+        <ListaP Nombre="parqueadero público" Direccion="85, Cra. 16 #39 A " Cupos=" 3"/>
+        <ListaP Nombre="Parqueadero Cl48" Direccion="Cl. 48 #976 " Cupos=" 2"/>
+        <ListaP Nombre="Parking" Direccion="11, Cra. 52 #24" Cupos=" 1"/>
+        <ListaP Nombre="Parqueadero 24 Horas Cl55" Direccion="Cra. 13 #21 " Cupos=" 8"/>
+        <ListaP Nombre="Parqueadero Autolemus" Direccion="Cl. 64 #11-31 " Cupos=" 9"/>
+        <ListaP Nombre="Parqueadero Público Parking" Direccion="51, Cl. 52 #7 " Cupos=" 1"/>
+        <ListaP Nombre="Parqueadero Público Park Elite S.A" Direccion="53, Cra. 7 #52 " Cupos=" 8"/>
+        <ListaP Nombre="Parqueadero J.S.L." Direccion="Cra. 7 #41-45 " Cupos=" 9"/>
+        <ListaP Nombre="     " Direccion="calle " Cupos=" 1"/>
+        <ListaP Nombre="     " Direccion="calle " Cupos=" 8"/>
+        <ListaP Nombre="     " Direccion="calle " Cupos=" 9"/>
+  </div>
+);
+
 
 export default App;
